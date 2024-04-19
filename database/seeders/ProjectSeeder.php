91b5323add5de9b63c4dccfd1fdccdb595bf67c6
MLaz95 <lazzari78.ml@gmail.com>
2024-04-19 12:42:16 +0200

    implementato faker nel ProjectSeeder e aggiornato route service provider

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,19 +6,23 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
 
+use Faker\Generator as Faker;
+
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $newProject = new Project();
-        $newProject->name = 'test';
-        $newProject->description = 'testtest';
-        $newProject->cover = 'testtest';
-        $newProject->tech = 'testtest';
-        $newProject->github = 'testtest';
-        $newProject->save();
+        for($i = 0; $i < 10; $i++){
+            $newProject = new Project();
+            $newProject->name = $faker->word();
+            $newProject->description = $faker->paragraph();
+            $newProject->cover = $faker->imageUrl(640, 480, 'a website', true);;
+            $newProject->tech = $faker->words(rand(1, 3), true);
+            $newProject->github = $faker->url();
+            $newProject->save();
+        }
     }
 }
